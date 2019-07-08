cec798b58a6d83c4d7978c7198f3c6347c686469
JanoChacon <a.chacon01@ufromail.cl>
2019-07-08 15:41:49 -0400

    actualizacion home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,21 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-body">
-                            <h5 class="card-title">Bienvenido</h5>
+                            <h5 class="card-title"><strong>Bienvenido </strong> {{ Auth::user()->name }}</h5>
+                            <br>
+                            <p>En esta sección se muestran los cuestionarios actuales con los que cuenta el prototipo,
+                                recuerde leer las instrucciones iniciales de cada uno para no tener interrupciones al resolver.</p>
                             <br>
                             <br>
-                            <br>
-                            <a href="/cuestionario" class="btn btn-primary btn-lg btn-block">Cuestionario de prueba 1</a>
+                            <ul class="list-group">
+                                    <li class="list-group-item list-group-item-primary">
+                                        <a href="/cuestionario" class="btn btn-link">Cuestionario de prueba 1</a>
+                                    </li>
+                                    <li class="list-group-item list-group-item-secondary">
+                                        <a href="#" class="btn btn-link">Cuestionario de prueba 2</a>
+                                    </li>
+                                  </ul>
+
                     </div>
                 </div>
             </div>
